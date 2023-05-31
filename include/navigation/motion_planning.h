3b3ddeb2a793816d8e3933cd580b9701810709ef
yaozhuo <1521232476@qq.com>
2023-05-31 11:56:54 +0800

    add basic element

---
+++ include/navigation/motion_planning.h
@@ -0,0 +1,15 @@
+#include "FaboRobot.h"
+
+void motion_planning() {
+    // visit data for motion 
+    FaboRobot::instance().global_path;
+    FaboRobot::instance().pose;
+    FaboRobot::instance().map;
+    // update velocity command
+    // implement DWA/TEB motion planner
+    
+    // update mission state
+    FaboRobot::instance().velocity;
+    Fabe
+    return SUCCESS;// ERROR
+};
